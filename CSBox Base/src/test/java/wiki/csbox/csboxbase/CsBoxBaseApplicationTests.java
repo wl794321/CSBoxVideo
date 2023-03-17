98efb5f86f2b436308dd398ca637a5941692c685
Krian <2793260947@qq.com>
2023-03-17 11:05:54 +0800

    ✏完成父工程和Base工程的创建和pom.xml文件的配置！ 2023-03-17

---
+++ CSBox Base/src/test/java/wiki/csbox/csboxbase/CsBoxBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package wiki.csbox.csboxbase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsBoxBaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
